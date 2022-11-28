687179b1d24ac44f9c9b3b97e50383640c66fa56
fabioVitoTavares <fabio.furtado@estudante.ufla.br>
2022-11-27 23:53:56 -0300

    refatoring

--- Tasks/src/Components/Task/index.tsx
+++ Tasks/src/Components/Task/index.tsx
@@ -1,11 +1,16 @@
 import './style.css'
+import { BsInfoSquare, BsCheckSquare, BsTrash } from 'react-icons/bs'
 
 export function Task() {
   
   return (
-    <div className="task">
-
-      <h1>Task 1</h1>
+    <div className='task'>
+      <div className='options'>
+        <BsTrash className='buttonTrash'/>
+        <BsInfoSquare className='buttonInfo'/>
+        <BsCheckSquare className='buttonCheck' />
+      </div>
+      <h2 className='descriptionTask'>Taks </h2>
 
     </div>
     
